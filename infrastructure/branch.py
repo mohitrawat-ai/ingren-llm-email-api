21935728a1ea688019f90b388426bc2702502aba
mohit rawat <mohit.rawat@bizense.com>
2025-06-03 21:52:13 +0530

    Break pulumi file into multiple aliases

---
+++ infrastructure/branch.py
@@ -0,0 +1,176 @@
+from pulumi_aws import lambda_, apigateway
+import pulumi
+import pulumi_command as command
+
+
+class Branch:
+    def __init__(self, name, lambda_function, version):
+        self.name = name
+        self.is_main_branch = name == "main"
+        self.lambda_function = lambda_function
+        self.version = version
+
+    @staticmethod
+    def get_alias_version_safe(function_name, alias_name, default="1"):
+        """Safely get alias version, return default if doesn't exist"""
+        cmd = command.local.Command(
+            f"get-alias-{alias_name}",
+            create=f"""
+            VERSION=$(aws lambda get-alias \
+              --function-name {function_name} \
+              --name {alias_name} \
+              --query 'FunctionVersion' \
+              --output text 2>/dev/null)
+
+            if [ "$?" -eq 0 ] && [ "$VERSION" != "None" ]; then
+                echo "$VERSION"
+            else
+                echo "{default}"
+            fi
+            """
+        )
+        return cmd.stdout.apply(lambda v: v.strip())
+
+    def create_alias(self, current_branch):# For feature branches - always update alias to current $LATEST
+        alias_name = f"{self.lambda_function.id}-{self.name}-alias"
+        current_version = Branch.get_alias_version_safe(self.lambda_function.name, alias_name)
+        function_version = current_version if self.name == current_branch else self.lambda_function.version
+        lambda_alias = lambda_.Alias(
+            alias_name,
+            function_name=self.lambda_function.name,
+            function_version=function_version,  # Points to current $LATEST at creation time
+            name=self.name,
+        )
+        return lambda_alias
+
+    def create_api_gateway_resources(self, rest_api, parent_id, lambda_alias, lambda_function):
+        # Create a version resource under api
+        v_resource = apigateway.Resource(
+            f"version-resource-{self.name}",
+            rest_api=rest_api.id,
+            parent_id=parent_id,
+            path_part=self.version,
+        )
+        # Create only the generate-email resource
+        generate_email_resource = apigateway.Resource(
+            f"generate-email-resource-{self.name}",
+            rest_api=rest_api.id,
+            parent_id=v_resource.id,
+            path_part="generate-email",
+        )
+
+        # Create the POST method for generate-email
+        generate_email_method = apigateway.Method(
+            f"generate-email-method-{self.name}",
+            rest_api=rest_api.id,
+            resource_id=generate_email_resource.id,
+            http_method="POST",
+            authorization="NONE",
+            api_key_required=True  # Require API key for this method
+        )
+
+        # Create integration between the API Gateway method and Lambda
+        generate_email_integration = apigateway.Integration(
+            f"generate-email-integration-{self.name}",
+            rest_api=rest_api.id,
+            resource_id=generate_email_resource.id,
+            http_method=generate_email_method.http_method,
+            integration_http_method="POST",
+            type="AWS_PROXY",
+            uri=pulumi.Output.concat(self.lambda_function.invoke_arn, ":", lambda_alias.name),
+        )
+
+        # Set up CORS for the generate-email endpoint
+        cors_options_method = apigateway.Method(
+            f"generate-email-options-{self.name}",
+            rest_api=rest_api.id,
+            resource_id=generate_email_resource.id,
+            http_method="OPTIONS",
+            authorization="NONE",
+        )
+
+        cors_options_integration = apigateway.Integration(
+            f"generate-email-options-integration-{self.name}",
+            rest_api=rest_api.id,
+            resource_id=generate_email_resource.id,
+            http_method=cors_options_method.http_method,
+            type="MOCK",
+            request_templates={
+                "application/json": '{"statusCode": 200}'
+            },
+        )
+
+        # Set up responses for OPTIONS method
+        options_method_response = apigateway.MethodResponse(
+            "options-method-response" + self.name,
+            rest_api=rest_api.id,
+            resource_id=generate_email_resource.id,
+            http_method="OPTIONS",
+            status_code="200",
+            response_models={
+                "application/json": "Empty",
+            },
+            response_parameters={
+                "method.response.header.Access-Control-Allow-Headers": True,
+                "method.response.header.Access-Control-Allow-Methods": True,
+                "method.response.header.Access-Control-Allow-Origin": True,
+            },
+        )
+
+        options_integration_response = apigateway.IntegrationResponse(
+            f"options-integration-response-{self.name}",
+            rest_api=rest_api.id,
+            resource_id=generate_email_resource.id,
+            http_method="OPTIONS",
+            status_code="200",
+            response_parameters={
+                "method.response.header.Access-Control-Allow-Headers": "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+                "method.response.header.Access-Control-Allow-Methods": "'OPTIONS,POST'",
+                "method.response.header.Access-Control-Allow-Origin": "'*'",
+            },
+            selection_pattern="",
+            opts=pulumi.ResourceOptions(depends_on=[options_method_response]),
+
+        )
+
+        # Set up response for POST method
+        post_method_response = apigateway.MethodResponse(
+            f"post-method-response-{self.name}",
+            rest_api=rest_api.id,
+            resource_id=generate_email_resource.id,
+            http_method="POST",
+            status_code="200",
+            response_models={
+                "application/json": "Empty",
+            },
+            response_parameters={
+                "method.response.header.Access-Control-Allow-Origin": True,
+            },
+        )
+
+        # Grant API Gateway permission to invoke Lambda
+        lambda_permission = lambda_.Permission(
+            f"api-gateway-lambda-permission-{self.name}",
+            action="lambda:InvokeFunction",
+            function=lambda_function.name,
+            principal="apigateway.amazonaws.com",
+            qualifier=lambda_alias.name,
+            source_arn=pulumi.Output.concat(
+                rest_api.execution_arn, "/*/*"
+            ),
+        )
+
+        resources = [
+            v_resource,
+            generate_email_resource,
+            generate_email_method,
+            generate_email_integration,
+            cors_options_method,
+            cors_options_integration,
+            options_method_response,
+            options_integration_response,
+            post_method_response,
+            lambda_permission
+        ]
+
+        return resources
